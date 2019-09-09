d669c28588c51c6531a0c5dbc08dc1f18efd0a8b
Huy Mai <huy.mai@ericsson.com>
2019-09-09 18:07:25 +0300

    Commit

--- main.go
+++ main.go
@@ -32,10 +32,16 @@ func main() {
     // pdf viewer's name is "Document Viewer"
     var csv_file = flag.String("csv_file", "problems.csv",
                                "Path to the data csv file")
+    var time_limit = flag.Int("time_limit", 30, "Time allowed")
     flag.Parse()
-    fmt.Printf("%q\n", *csv_file)
     reader := bufio.NewReader(os.Stdin)
     var correct int
+    timer := time.NewTimer(time.Second*time_limit)
+    fmt.Print("Press Enter to start...")
+    _, _ := reader.ReadString('\n')
+    go func() {
+        <-timer.c
+
     questions, answers := read_csv(*csv_file)
     for i := range questions {
         fmt.Print(questions[i],":")
