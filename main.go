9ef5db8a98c216680950f39a6d2cb4753c672e93
Huy Mai <huy.mai@ericsson.com>
2019-09-09 16:13:39 +0300

    Finish part 1

--- main.go
+++ main.go
@@ -1,31 +1,48 @@
 package main
 import(
-	"fmt"
-	"encoding/csv"
-	"io"
-	"io/ioutil"
-	"strings"
+    "bufio"
+    "flag"
+    "fmt"
+    "encoding/csv"
+    "io"
+    "io/ioutil"
+    "os"
+    "strings"
 )
 
+
 func read_csv(csv_file string) ([]string, []string) {
-	dat, _ := ioutil.ReadFile(csv_file)
-	r := csv.NewReader(strings.NewReader(string(dat)))
-	var questions []string
-	var answers []string
-	for {
-		record, err := r.Read()
-		if err == io.EOF {
-			break
-		}
-		questions = append(questions, record[0])
-		answers = append(answers, record[1])
-	}
-	return questions, answers
+    dat, _ := ioutil.ReadFile(csv_file)
+    r := csv.NewReader(strings.NewReader(string(dat)))
+    var questions []string
+    var answers []string
+    for {
+        record, err := r.Read()
+        if err == io.EOF {
+            break
+        }
+        questions = append(questions, record[0])
+        answers = append(answers, record[1])
+    }
+    return questions, answers
 }
 
+
 func main() {
-	csv_file := "problems.csv"
-	questions, answers := read_csv(csv_file)
-	fmt.Printf("%q\n", questions)
-	fmt.Printf("%q\n", answers)
+    // pdf viewer's name is "Document Viewer"
+    var csv_file = flag.String("csv_file", "problems.csv",
+                               "Path to the data csv file")
+    flag.Parse()
+    fmt.Printf("%q\n", *csv_file)
+    reader := bufio.NewReader(os.Stdin)
+    var correct int
+    questions, answers := read_csv(*csv_file)
+    for i := range questions {
+        fmt.Print(questions[i],":")
+        answer, _ := reader.ReadString('\n')
+        if strings.Compare(answer, answers[i]) == 0 {
+            correct ++;
+        }
+    }
+    fmt.Printf("%d/%d\n", correct, len(questions))
 }
