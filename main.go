bcdda364181868e0c303c8c03edfa6a674d3c20c
Huy Mai <mquhuy@gmail.com>
2019-09-09 22:35:50 +0300

    func1

--- main.go
+++ main.go
@@ -29,25 +29,23 @@ func read_csv(csv_file string) ([]string, []string) {
 
 
 func main() {
-    // pdf viewer's name is "Document Viewer"
     var csv_file = flag.String("csv_file", "problems.csv",
                                "Path to the data csv file")
     var time_limit = flag.Int("time_limit", 30, "Time allowed")
     flag.Parse()
     reader := bufio.NewReader(os.Stdin)
     var correct int
+    questions, answers := read_csv(*csv_file)
     timer := time.NewTimer(time.Second*time_limit)
     fmt.Print("Press Enter to start...")
     _, _ := reader.ReadString('\n')
     go func() {
-        <-timer.c
-
-    questions, answers := read_csv(*csv_file)
-    for i := range questions {
-        fmt.Print(questions[i],":")
-        answer, _ := reader.ReadString('\n')
-        if strings.Compare(answer, answers[i]) == 0 {
-            correct ++;
+        for i := range questions {
+            fmt.Print(questions[i],":")
+            answer, _ := reader.ReadString('\n')
+            if strings.Compare(answer, answers[i]) == 0 {
+                correct ++;
+            }
         }
     }
     fmt.Printf("%d/%d\n", correct, len(questions))
